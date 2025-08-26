8e52566c21dc6a1d33ff84d0bfe89fd8169b4e58
chandan <chandan@bitmaxgroup.com>
2025-08-26 18:50:39 +0530

    changes in customer frontend part

--- src/apps/store.js
+++ src/apps/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from '../features/auth/authApi';
+import { chatApi } from '../features/chat/chatApi';
 
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+     [chatApi.reducerPath]: chatApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware)
+      .concat(chatApi.middleware),
 });
--- src/pages/private/customer/Home.jsx
+++ src/pages/private/customer/Home.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+// Home.js
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import SpeedDial from "@mui/material/SpeedDial";
+import { Chat } from "@mui/icons-material";
+import ChatBot from "./customerChat/ChatBot";
+import { jwtDecode } from "jwt-decode";
+import CustomerDashboard from "./CustomerDashboard";
 
 const Home = () => {
+  const [showChat, setShowChat] = useState(false);
+ const token = localStorage.getItem("token");
+  const decoded = token ? jwtDecode(token) : null;
+  const customerUserId = decoded?.id;
   return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+    <>
+    <CustomerDashboard/>
+ 
+    <Box sx={{ height: "87.7vh", flexGrow: 1, position: "relative" }}>
+
+      {/* ChatBot Window */}
+      {showChat && (
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)", // center horizontally & vertically
+            zIndex: 999,
+          }}
+        >
+          <ChatBot />
+        </Box>
+      )}
+
+
+      {/* Floating Chat Icon */}
+      <SpeedDial
+        ariaLabel="Chat"
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<Chat />}
+        onClick={() => setShowChat((prev) => !prev)}
+      />
+    </Box>
+       </>
+  );
+};
+
+export default Home;
